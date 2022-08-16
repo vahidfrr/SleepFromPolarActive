2dc7fa3b1410945b613a626efc845928ffa3b8d2
sfarrahi <sfarrahi@OY2009078.univ.yo.oulu.fi>
2022-08-16 10:33:06 +0300

    Updates in the code

--- ExtractSleep.m
+++ ExtractSleep.m
@@ -36,11 +36,9 @@ for k=2:length(DateList)-1 %First day is always excluded
     Times_OneDay=Time(OneDayIndices);
     
     % Create a Nan vector for visualization purposes.
-    % These Nan vectors will be used to show short activity bouts,
-    % potential non-wear time and rest/sleep period
-    vector_ShortActivityBouts=NaN(length(METValues_OneDay),1); 
+    % These Nan vectors will be used to show rest/sleep period
     vector_RestBouts=NaN(length(METValues_OneDay),1); 
-    vector_SuspectedNonWear=NaN(length(METValues_OneDay),1); 
+
     
     
     %%%%%Check for non-wear. All bouts more than 2.5 hours with measurement
@@ -58,13 +56,13 @@ for k=2:length(DateList)-1 %First day is always excluded
     end
     
 
-    % Identify sustained activity bouts > 10 minutes
+    % Identify sustained activity bouts > 45 minutes
     METValues_OneDay_Indices_ABOVE1MET=(METValues_OneDay>=1);
     CosecutiveOnes_Indices_ABOVE1MET = find(diff([0;(METValues_OneDay_Indices_ABOVE1MET);0]==1));
     StartIndices = CosecutiveOnes_Indices_ABOVE1MET(1:2:end-1);  % Start indices
     EndIndices=CosecutiveOnes_Indices_ABOVE1MET(2:2:end);  % End indices
     ConsecutiveOnes_Count_ABOVE1MET = EndIndices-StartIndices;  % Consecutive ones counts
-    Bout_indicesAbove10Min=ConsecutiveOnes_Count_ABOVE1MET>=20; % mark all the bouts that have MET values >=1 for 10 consecutive minutes or more
+    Bout_indicesAbove10Min=ConsecutiveOnes_Count_ABOVE1MET>=90; % mark all the bouts that have MET values >=1 for 45 consecutive minutes or more
    
     StartIndices_Above10Min_1=[nan;StartIndices(Bout_indicesAbove10Min)];
   	EndIndices_Above10Min_1=[nan;EndIndices(Bout_indicesAbove10Min)];
@@ -76,32 +74,22 @@ for k=2:length(DateList)-1 %First day is always excluded
 
     % Go through all identified bouts 
     for i=2:length(StartIndices_Above10Min_2)-1
-
-        
-        %%%%%%%___Mark the short movement bouts that were considered as part of sleep%%%%%%%%
-        % In other words, if there was some movement every now and then (lasting for 10 minutes) but was not sustained
-        % between sustaind activities, they were combined marked as
-        % bedtime.
-        if(ActiveBoutlength(i)<90) 
-            vector_ShortActivityBouts(StartIndices_Above10Min_1(i):EndIndices_Above10Min_1(i)-1)=2.5;
-
-        end
         vector_RestBouts(EndIndices_Above10Min_2(i)-1:StartIndices_Above10Min_2(i))=1.5;       
     end
     
     
     % Combine Rest/bed periods with short active bouts
-    Empty_vector_RestBouts_ConcatenateWithActive=(~isnan(vector_RestBouts)|~isnan(vector_ShortActivityBouts));
-    Empty_vector_RestBouts_Indices=(Empty_vector_RestBouts_ConcatenateWithActive==1);
-    CosecutiveOnes_Indices_ABOVE1MET = find(diff([0;(Empty_vector_RestBouts_Indices);0]==1));
+    RestBouts=(~isnan(vector_RestBouts));
+    RestBouts_Indices=(RestBouts==1);
+    CosecutiveOnes_Indices_ABOVE1MET = find(diff([0;(RestBouts_Indices);0]==1));
     StartIndices = CosecutiveOnes_Indices_ABOVE1MET(1:2:end-1);  % Start indices
     EndIndices=CosecutiveOnes_Indices_ABOVE1MET(2:2:end);  % End indices
     ConsecutiveOnes_Count_ABOVE1MET = EndIndices-StartIndices;  % Consecutive ones counts
     [Bout_Longest_Values, Bout_Longest_Indices]=max(ConsecutiveOnes_Count_ABOVE1MET); %Find the longest bout
     
     % create a Nan vector for marking time in bed periods on the figure
-    vector_RestBouts_Longest=NaN(length(METValues_OneDay),1); 
-    vector_RestBouts_Longest(StartIndices(Bout_Longest_Indices):EndIndices(Bout_Longest_Indices))=3;
+    RestBouts_Longest=NaN(length(METValues_OneDay),1); 
+    RestBouts_Longest(StartIndices(Bout_Longest_Indices):EndIndices(Bout_Longest_Indices))=3;
     
     
     
@@ -124,8 +112,7 @@ for k=2:length(DateList)-1 %First day is always excluded
     plot (TimeVectorForFigure(1:T_length),METValues_OneDay)
     hold on;
     plot(TimeVectorForFigure(1:T_length),vector_RestBouts,...
-    TimeVectorForFigure(1:T_length),vector_ShortActivityBouts,...
-    TimeVectorForFigure(1:T_length),vector_RestBouts_Longest,'LineWidth',3)
-    legend('Met values','inactivity periods','Short movement periods', 'Sleep period (longest inactive bout)')
+    TimeVectorForFigure(1:T_length),RestBouts_Longest,'LineWidth',3)
+    legend('Met values','Potential sleep periods', 'Sleep period (longest inactive bout)')
     
 end
